62e3ba8aa35c3192d18d0bcdd1a0078034c8e24c
Shaman Ando <you@example.com>
2022-09-23 13:35:22 +0400

    add Homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,26 @@
+﻿int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
+
+int ReadInputData(string message)
+{
+    Console.WriteLine(message);
+    if (!int.TryParse(Console.ReadLine(), out var result))
+        Console.WriteLine("Все плохо");
+    return result;
+}
+int m = ReadInputData("Введите число M");
+int n = ReadInputData("Введите число N");
+Console.Write($"\nm = {m}, n = {n} -> A(m,n) = {Akkerman(m, n)}");
