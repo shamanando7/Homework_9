531b61d4601adeed4617a9d547bb5253d5810120
Shaman Ando <you@example.com>
2022-09-23 13:31:20 +0400

    add Homeworkl

---
+++ Homework1/Program.cs
@@ -0,0 +1,22 @@
+﻿void PrintNaturalNumber(int m, int n)
+{
+    if (m == n) Console.Write($"{n} ");
+    else
+    {
+        Console.Write($"{m} ");
+        PrintNaturalNumber(m + 1, n);
+        
+    }
+    
+}
+
+int ReadInputData(string message)
+{
+    Console.WriteLine(message);
+    if (!int.TryParse(Console.ReadLine(), out var result))
+        Console.WriteLine("Все плохо");
+    return result;
+}
+int m = ReadInputData("Введите число M");
+int n = ReadInputData("Введите число N");
+PrintNaturalNumber(m, n);
