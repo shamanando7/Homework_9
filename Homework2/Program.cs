34ebe35cc32e4c9a657d344392c8e43ec7533e81
Shaman Ando <you@example.com>
2022-09-23 13:34:02 +0400

    add Homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,33 @@
+﻿void SumFromMtoN(int m, int n, int sum = 0)
+{
+    if (m > n)
+    {
+        Console.WriteLine($" \nСумма от M до N: {sum}");
+        return;
+    }
+    sum = sum + (m++);
+    Console.Write($"{sum} ");
+    SumFromMtoN(m, n, sum);
+}
+
+int ReadInputData(string message)
+{
+    Console.WriteLine(message);
+    if (!int.TryParse(Console.ReadLine(), out var result))
+        Console.WriteLine("Все плохо");
+    return result;
+}
+int m = ReadInputData("Введите число M");
+int n = ReadInputData("Введите число N");
+int temp = m;
+if (n == m)
+{
+    Console.WriteLine("0");
+    return;
+}
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+SumFromMtoN(m, n);
